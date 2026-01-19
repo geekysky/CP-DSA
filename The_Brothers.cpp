6a4ea6b9c3e8afa46b6cd37b829bfa8e8f0f2d97
geekysky <debu69980@gmail.com>
2026-01-19 22:16:03 +0530

    Add implementation for "The Brothers" problem

---
+++ The_Brothers.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+signed main(){
+    string a,b,c,d;
+    cin>>a>>b;
+    cin>>c>>d;
+
+    string ans = (b==d?"ARE Brothers":"NOT");
+
+    cout<<ans<<endl;
+    return 0;
+}
